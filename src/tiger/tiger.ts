768dee5082432a2243470b8b4b655c470d8adb4a
sunday00 <kind65505256@gmail.com>
2024-01-23 23:19:37 +0900

    :sparkles: tiger

--- src/tiger/tiger.ts
+++ src/tiger/tiger.ts
@@ -1,9 +1,19 @@
 import * as Charm from 'charm'
+import * as fs from 'fs'
+import * as path from 'path'
 
 const charm = Charm()
-charm.pipe(process.stdout)
+charm.pipe(process['stdout'])
 
+const frames = []
+const folder = fs.readdirSync('./src/tiger/frames')
+for(const filePath of folder) {
+  frames.push(fs.readFileSync(path.join('./src/tiger/frames', filePath), 'utf8'))
+}
+
+let i = 0
 setInterval(() => {
   charm.reset()
-  charm.write('*'.repeat(Math.floor(Math.random() * 10)))
+  charm.write(frames[i % frames.length])
+  i++
 }, 200)
