84d6f16a49d49092d7e734cc8ef4bf305a89b3ab
Andrea Kim <beomyeon.kim@adriel.ai>
2024-01-25 20:39:41 +0900

    feat: add rabbit

---
+++ src/rabbit/rabbit.ts
@@ -0,0 +1,19 @@
+import * as Charm from 'charm'
+import * as fs from 'fs'
+import * as path from 'path'
+
+const charm = Charm()
+charm.pipe(process['stdout'])
+
+const frames = []
+const folder = fs.readdirSync('src/rabbit/frames')
+for(const filePath of folder) {
+  frames.push(fs.readFileSync(path.join('src/rabbit/frames', filePath), 'utf8'))
+}
+
+let i = 0
+setInterval(() => {
+  charm.reset()
+  charm.write(frames[i % frames.length])
+  i++
+}, 200)
