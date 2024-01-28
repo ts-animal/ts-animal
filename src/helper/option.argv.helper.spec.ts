09897ec5ffb897cf569885651fb6efd7d61e57bb
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-01-28 11:18:27 +0900

    feat: test helpers

--- src/helper/option.argv.helper.spec.ts
+++ src/helper/option.argv.helper.spec.ts
@@ -9,4 +9,21 @@ describe('OptionArgvHelper', () => {
 
     expect(result).toEqual({ speed: '200' });
   });
+
+  test('it should parse multiple arguments correctly', () => {
+    const argv = ['--speed=200', '--repeat=3'];
+
+    const result: OptionType = OptionArgvHelper(argv);
+
+    expect(result).toEqual({ speed: '200', repeat: '3' });
+  });
+
+  // on progress on #6
+  // test('it should skip no-option arguments', () => {
+  //   const argv = ['--speed=200', '--repeat=3', '--cat=meow'];
+
+  //   const result: OptionType = OptionArgvHelper(argv);
+
+  //   expect(result).toEqual({ speed: '200', repeat: '3' });
+  // });
 });
