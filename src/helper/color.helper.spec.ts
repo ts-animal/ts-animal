ebb5ae8985c5109e4ea0f552859bcd068876a998
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-02-03 23:40:59 +0900

    feat: udpate test code, replace mocking values with the real responses of functions

--- src/helper/color.helper.spec.ts
+++ src/helper/color.helper.spec.ts
@@ -1,19 +1,18 @@
-import { COLOR, listColors, getRandomColor } from '../helper/color.helper';
+import { COLOR, getRandomColor, listColors } from '../helper/color.helper';
 
 describe('Color Module', () => {
-  const mockListColors = listColors();
-  const mockRandomColor = '\x1b[31m';
-
   test('getRandomColor should return a valid color', () => {
-    expect(mockRandomColor).toMatch(/\x1b\[\d+m/); // ANSI color escape code
+    const randColor = getRandomColor();
+    expect(randColor).toMatch(/\x1b\[\d+m/); // ANSI color escape code
   });
 
   test('listColors should return a string with all colors', () => {
+    const result = listColors();
     const colorKeys = Object.keys(COLOR)
       .filter((e) => e !== 'reset')
       .map((e) => `${COLOR[e]}${e}${COLOR.reset}`)
       .join(', ');
 
-    expect(mockListColors).toMatch(colorKeys);
+    expect(result).toMatch(colorKeys);
   });
 });
